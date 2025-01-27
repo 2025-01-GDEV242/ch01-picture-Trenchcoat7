f6d3f5230135a31de822464340ade88f88d8df6f
Katie <157065653+Trenchcoat7@users.noreply.github.com>
2025-01-27 12:10:22 -0500

    I made shapes :3

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Person dude;
     private boolean drawn;
 
     /**
@@ -26,6 +27,7 @@ public class Picture
         window = new Square();
         roof = new Triangle();  
         sun = new Circle();
+        dude = new Person();
         drawn = false;
     }
 
@@ -56,6 +58,12 @@ public class Picture
             sun.moveVertical(-40);
             sun.changeSize(80);
             sun.makeVisible();
+            
+            dude.changeColor("black");
+            dude.moveHorizontal(100);
+            dude.moveVertical(0);
+            dude.makeVisible();
+            
             drawn = true;
         }
     }
